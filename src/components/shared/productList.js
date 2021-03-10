60cba9186756c8b6b77197ef962ab82c5dc96944
vicc23@gmail.com <vicc23@gmail.com>
2021-03-10 17:15:48 -0600

    Optimization gh-pages

--- src/components/shared/productList.js
+++ src/components/shared/productList.js
@@ -2,7 +2,7 @@ export const PRODUCTS = [
     {
         id: "111",
         name: 'Pepsi',
-        image: '/assets/images/b1.png',
+        image: './assets/images/b1.png',
         type: "beverages",
         price: 0.50,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia. Duis labore sit voluptate dolore ad ipsum labore quis labore exercitation laboris. Enim est velit eu sit veniam aliqua aute ea enim esse consequat. Consequat cupidatat sint aliquip magna qui."
@@ -10,7 +10,7 @@ export const PRODUCTS = [
     {
         id: "112",
         name: 'Gatorade',
-        image: '/assets/images/b2.png',
+        image: './assets/images/b2.png',
         type: "beverages",
         price: 1.25,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia. Duis labore sit voluptate dolore ad ipsum labore quis labore exercitation laboris. Enim est velit eu sit veniam aliqua aute ea enim esse consequat. Consequat cupidatat sint aliquip magna qui."
@@ -18,7 +18,7 @@ export const PRODUCTS = [
     {
         id: "113",
         name: 'Tropicana',
-        image: '/assets/images/b3.png',
+        image: './assets/images/b3.png',
         type: "beverages",
         price: 2.97,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia. Duis labore sit voluptate dolore ad ipsum labore quis labore exercitation laboris. Enim est velit eu sit veniam aliqua aute ea enim esse consequat. Consequat cupidatat sint aliquip magna qui."
@@ -26,7 +26,7 @@ export const PRODUCTS = [
     {
         id: "114",
         name: 'Lipton',
-        image: '/assets/images/b4.png',
+        image: './assets/images/b4.png',
         type: "beverages",
         price: 0.50,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -34,7 +34,7 @@ export const PRODUCTS = [
     {
         id: "115",
         name: 'Frapuccino',
-        image: '/assets/images/b5.png',
+        image: './assets/images/b5.png',
         type: "beverages",
         price: 2.97,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia"
@@ -42,7 +42,7 @@ export const PRODUCTS = [
     {
         id: "116",
         name: 'Mug Root Beer',
-        image: '/assets/images/b6.png',
+        image: './assets/images/b6.png',
         type: "beverages",
         price: 0.50,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -50,7 +50,7 @@ export const PRODUCTS = [
     {
         id: "117",
         name: 'Life wtr',
-        image: '/assets/images/b7.png',
+        image: './assets/images/b7.png',
         type: "beverages",
         price: 2.47,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -58,7 +58,7 @@ export const PRODUCTS = [
     {
         id: "118",
         name: 'Pepsi Light',
-        image: '/assets/images/b8.png',
+        image: './assets/images/b8.png',
         type: "beverages",
         price: 0.50,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -66,7 +66,7 @@ export const PRODUCTS = [
     {
         id: "fd1",
         name: "Lay's",
-        image: '/assets/images/f1.png',
+        image: './assets/images/f1.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -74,7 +74,7 @@ export const PRODUCTS = [
     {
         id: "fd2",
         name: "Doritos",
-        image: '/assets/images/f2.png',
+        image: './assets/images/f2.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -82,7 +82,7 @@ export const PRODUCTS = [
     {
         id: "fd3",
         name: "Quaker",
-        image: '/assets/images/f3.png',
+        image: './assets/images/f3.png',
         type: "food",
         price: 2.97,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -90,7 +90,7 @@ export const PRODUCTS = [
     {
         id: "fd4",
         name: "Sabra",
-        image: '/assets/images/f4.png',
+        image: './assets/images/f4.png',
         type: "food",
         price: 3.97,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -98,7 +98,7 @@ export const PRODUCTS = [
     {
         id: "fd5",
         name: "Ruffles",
-        image: '/assets/images/f5.png',
+        image: './assets/images/f5.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -106,7 +106,7 @@ export const PRODUCTS = [
     {
         id: "fd6",
         name: "Smart 50",
-        image: '/assets/images/f6.png',
+        image: './assets/images/f6.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -114,7 +114,7 @@ export const PRODUCTS = [
     {
         id: "fd7",
         name: "Cheetos",
-        image: '/assets/images/f7.png',
+        image: './assets/images/f7.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -122,7 +122,7 @@ export const PRODUCTS = [
     {
         id: "fd8",
         name: "Tostitos ",
-        image: '/assets/images/f8.png',
+        image: './assets/images/f8.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -130,7 +130,7 @@ export const PRODUCTS = [
     {
         id: "fd9",
         name: "Fritos ",
-        image: '/assets/images/f9.png',
+        image: './assets/images/f9.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -138,7 +138,7 @@ export const PRODUCTS = [
     {
         id: "fd10",
         name: "Aunt Jemima",
-        image: '/assets/images/f10.png',
+        image: './assets/images/f10.png',
         type: "food",
         price: 2.67,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia"
@@ -146,7 +146,7 @@ export const PRODUCTS = [
     {
         id: "fd11",
         name: "Cap'n Crunch",
-        image: '/assets/images/f11.png',
+        image: './assets/images/f11.png',
         type: "food",
         price: 3.47,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
@@ -154,7 +154,7 @@ export const PRODUCTS = [
     {
         id: "fd12",
         name: "Sunchips",
-        image: '/assets/images/f12.png',
+        image: './assets/images/f12.png',
         type: "food",
         price: 1.48,
         description: "Ad mollit eiusmod excepteur occaecat occaecat et eiusmod excepteur duis occaecat reprehenderit cupidatat sunt consequat. Veniam nulla non sunt elit labore labore pariatur aliqua aute nulla officia."
